07abfa50677fff571fe58a5b1228abc35c608b89
wsw1206 <wangsiwen1206@ucla.edu>
2015-11-19 00:51:54 +0800

    First commit

---
+++ animats/predator.py
@@ -0,0 +1,140 @@
+import sys
+sys.path.append("..")
+
+from environment.env import *
+from ffn import FFN
+import random
+
+class Predator(Object):
+
+	senserange = 80
+
+	def __init__(self, type, x=0, y=0):
+		Object.__init__(self, type, x, y)
+		self.brain = FFN()
+		self.step = 8
+		#print self.x, self.y
+		self.stroll = [random.choice(['up', 'down', 'left', 'right'])]
+		self.energy = 70
+
+	def tick(self, env):
+		params = self.sense(env)
+		result = self.brain.act(params)
+		if params[5] == 1:
+			if len(self.stroll)>=2:
+				self.act(self.stroll[-1], env)
+				self.stroll.pop()
+			else:
+				action = ['up', 'down', 'left', 'right']
+				re = ['down', 'up', 'right', 'left']
+				del action[re.index(self.stroll[0])]
+				self.stroll = [action[random.randrange(0,3)]]*random.randrange(50,100)
+			return
+		action = self.choseaction(result, env)
+		#if action == 'eat':
+			#print params, result, action
+		self.act(action, env)
+
+	def sense(self, env):
+		prey = env.find('prey', (self.x, self.y), Predator.senserange)
+		err = 2
+		result = [0, 0, 0, 0, 0, 0]
+		if prey != None and self.name not in prey.safe:
+			if abs(prey.y-self.y) <= err and abs(prey.x-self.x) <= err:
+				result[4] += 1 # on
+			else:
+				if prey.y + err < self.y:
+					result[0] += 1 # up
+				if prey.y - err > self.y:
+					result[1] += 1 # down
+				if prey.x + err < self.x:
+					result[2] += 1 # left
+				if prey.x - err > self.x:
+					result[3] += 1 # right
+		else:
+			result[5] += 1 # nothing	
+		return result
+
+	def choseaction(self, result, env):
+		actions = ['up', 'down', 'left', 'right', 'eat']
+		maxr = max(result)
+		list = []
+		for i in range(len(result)):
+			if result[i] > (maxr*0.8):
+				list.append(i)
+		if len(list)==4:
+			#list = list * 2
+			prey = env.find('prey', (self.x, self.y), diag)
+			if prey != None and self.name not in prey.safe:
+				if prey.y  < self.y:
+					list += [0]*5
+				if prey.y  > self.y:
+					list += [1]*5
+				if prey.x  < self.x:
+					list += [2]*5
+				if prey.x  > self.x:
+					list += [3]*5
+		return actions[random.choice(list)]
+
+	def act(self, action, env):
+		self.energy -= 0.7
+		if self.energy >= 70:
+			env.snake.append(Predator('snake'))
+		if self.energy <= 0:
+			env.snake.remove(self)
+			print 'snake die from food'
+		step = self.step
+		#if state == 6:
+		#	step = Prey.foodrange
+		if action == 'up':
+			self.y = self.y-step
+		if action == 'down':
+			self.y = self.y+step
+		if action == 'left':
+			self.x = self.x-step
+		if action == 'right':
+			self.x = self.x+step
+		self.adjustxy()
+		if action == 'eat':
+			self.eat(env)
+		#print self.x, self.y
+		#print action
+
+	def adjustxy(self):
+		constrain = 0
+		if self.x < constrain:
+			self.x = constrain
+			self.stroll = ['right']*2
+		if self.x > width-constrain:
+			self.x = width-constrain
+			self.stroll = ['left']*2
+		if self.y < constrain:
+			self.y = constrain
+			self.stroll = ['down']*2
+		if self.y > height-constrain:
+			self.y = height-constrain
+			self.stroll = ['up']*2
+
+	def eat(self, env):
+		prey = env.find('prey', (self.x, self.y), Predator.senserange)
+		
+		if prey!=None:
+
+			if self.name == 'snake':
+				self.energy += 15
+				env.prey.remove(prey)
+			elif random.random()>0.5:
+				self.energy += 15
+				env.prey.remove(prey)
+			else:
+				print 'half escape'
+			# env.remove(prey)
+			
+			# learning
+		#else :
+			#print 'Eat False'
+
+
+
+
+
---
+++ environment/env.py
@@ -0,0 +1,142 @@
+import math
+import random
+width = 800;
+height = 600;
+diag = 1000;
+
+class Object:
+  def __init__(self, name, x=0, y=0):
+    self.name = name
+    if x==0 and y==0:
+			self.setrandom()
+    else:
+			self.x = x
+			self.y = y
+    #print self.x, self.y
+  def setrandom(self):
+    self.x = int(random.random()*width)
+    self.y = int(random.random()*height)
+
+class Shelter(Object):
+  def __init__(self, name, x=0, y=0):
+    Object.__init__(self, name, x, y)
+    if name == 'bush':
+      self.shield = ['snake', 'hawk']
+    if name == 'hole':
+      self.shield = ['fox', 'hawk']
+    if name == 'tree':
+      self.shield = ['fox', 'snake']
+
+class Food(Object):
+	def __init__(self, x, y):
+		Object.__init__(self, 'food', x, y)
+
+class Environment:
+  def __init__(self):  
+    #self.map = {}
+    self.food = []
+    self.prey = []
+
+    self.hawk = []
+    self.fox = []
+    self.snake = []
+
+    self.bush = []
+    self.hole = []
+    self.tree = []
+
+  def add(self, type, x=0, y=0):
+  	if type == 'food':
+  		newfood = Food(x, y)
+  		self.food.append(newfood)
+
+        
+          
+
+
+  def remove(self, obj):
+    if obj.name == 'food':
+      #print 'remove food'
+      self.food.remove(obj)
+    if obj.name == 'prey':
+      print 'remove prey'
+      self.prey.remove(obj)
+    #else:
+  		#print 'dont have'
+  		
+  def find(self, type, center, range):
+    if type == 'food':
+  		list = self.food
+    elif type == 'prey':
+      list = self.prey
+    elif type == 'hawk':
+      list = self.hawk
+    elif type == 'bush':
+      list = self.bush
+    elif type == 'predator':
+      list = self.hawk+self.fox+self.snake
+    
+    target = None
+    pos = None
+    min_d = math.sqrt(width**2+height**2)
+    for obj in list:
+  	  x = center[0]-obj.x
+  	  y = center[1]-obj.y
+  	  d =  math.sqrt(x**2+y**2)
+  	  if d < min_d:
+  			target = obj
+  			min_d = d
+    if min_d>range:
+  		target = None
+    return target
+
+  def findall(self, type, center, range):
+    if type == 'hawk':
+      list = self.hawk
+    if type == 'fox':
+      list = self.fox
+    if type == 'snake':
+      list = self.snake
+    if type == 'food':
+      list = self.food
+
+    target = []
+    for obj in list:
+      x = center[0]-obj.x
+      y = center[1]-obj.y
+      d =  math.sqrt(x**2+y**2)
+      if d < range:
+        target.append(obj)
+    return target
+
+  def findshelter(self, type, center, predator, range):
+    if type == ['hawk']:
+      list = self.bush+self.hole
+    elif type == ['fox']:
+      list = self.hole+self.tree
+    elif type == ['snake']:
+      list = self.bush+self.tree
+    elif 'hawk' in type and 'fox' in type:
+      list = self.hole
+    elif 'hawk' in type and 'snake' in type:
+      list = self.bush
+    elif 'snake' in type and 'fox' in type:
+      list = self.tree
+
+    target = None
+    min_d = width+height
+    for obj in list:
+      x = center[0]-obj.x
+      y = center[1]-obj.y
+      d1 =  abs(x)+abs(y)
+      x = predator[0]-obj.x
+      y = predator[1]-obj.y
+      d2 =  abs(x)+abs(y)
+      if d1 < min_d and d1*8 < d2*9:
+        target = obj
+        min_d = d1
+    return target
+
+
+    #return None
+  
